33a5fb8a68290848e3ed94bac1de22560f38293d
Paulo Baena <jpbaena@gmail.com>
2018-03-04 21:12:17 +0100

    add: initial akka-http server endpoint with dummy response

--- db-server/build.sbt
+++ db-server/build.sbt
@@ -3,3 +3,18 @@ name := "db-server"
 version := "0.1"
 
 scalaVersion := "2.12.4"
+
+libraryDependencies ++= {
+  val akkaV       = "2.5.9"
+  val akkaHttpV   = "10.1.0-RC2"
+  val scalaTestV  = "3.0.5"
+  Seq(
+    "com.typesafe.akka" %% "akka-actor" % akkaV,
+    "com.typesafe.akka" %% "akka-stream" % akkaV,
+    "com.typesafe.akka" %% "akka-testkit" % akkaV,
+    "com.typesafe.akka" %% "akka-http" % akkaHttpV,
+    "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpV,
+    "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpV,
+    "org.scalatest"     %% "scalatest" % scalaTestV % "test"
+  )
+}
---
+++ db-server/src/main/scala/dbserver/DbServerApp.scala
@@ -0,0 +1,56 @@
+package dbserver
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+import akka.actor.ActorSystem
+import akka.event.{Logging, LoggingAdapter}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.{ActorMaterializer, Materializer}
+import com.typesafe.config.{Config, ConfigFactory}
+import spray.json.DefaultJsonProtocol
+import scala.concurrent.ExecutionContextExecutor
+
+case class DummyResponse(text: String)
+case class Table(name: String)
+
+trait Protocols extends DefaultJsonProtocol {
+  implicit val dummyFormat = jsonFormat1(DummyResponse)
+  implicit val tableFormat = jsonFormat1(Table)
+}
+
+trait Service extends Protocols {
+  implicit val system: ActorSystem
+
+  implicit def executor: ExecutionContextExecutor
+
+  implicit val materializer: Materializer
+
+  def config: Config
+
+  val logger: LoggingAdapter
+
+  val routes = {
+    logRequestResult("Query *") {
+      pathPrefix("table_contents") {
+        (post & entity(as[Table])) { tb =>
+          complete {
+            DummyResponse(s"Going fine.... Table: ${tb.name}")
+          }
+        }
+      }
+    }
+  }
+}
+
+object DbServerApp extends App with Service {
+  override implicit val system = ActorSystem()
+  override implicit val executor = system.dispatcher
+  override implicit val materializer = ActorMaterializer()
+  override val config = ConfigFactory.load()
+  override val logger = Logging(system, getClass)
+
+  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+}
